464a660e93c28fed8456c0a880bc6f299d72fee9
[marcelocd] <[marcelocdias93@gmail.com]>
2020-06-22 17:24:43 -0300

    Mudança de planos: segui outro tutorial. Tá funcionando. Agora é só deixar bonitinho.

--- main.js
+++
@@ -1,9 +0,0 @@
-const {app, BrowserWindow} = require('electron');
-var path = require('path');
-let mainWindow = null;
-
-app.on('ready',()=>{
-  mainWindow = new BrowserWindow({width: 800, height: 600, frame: false});
-  mainWindow.setMenu(null);
-  mainWindow.loadURL('file://' + __dirname + '/index.html'); 
-});
